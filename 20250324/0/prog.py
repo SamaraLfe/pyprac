8d501ebe2901d7b77bdde61e204d32712c5e488d
dmitry <frolov2013dm2@gmail.com>
2025-03-24 14:41:22 +0300

    cw

--- 20250324/0/prog.py
+++ 20250324/0/prog.py
@@ -1,1 +1,8 @@
-.
+import sys
+import socket
+from http.server import test, SimpleHTTPRequestHandler
+
+HOST = socket.gethostbyname(socket.gethostname())
+PORT = sys.argv[1] if len(sys.argv) > 1 else '8000'
+print(HOST)
+test(SimpleHTTPRequestHandler, port=int(PORT))
